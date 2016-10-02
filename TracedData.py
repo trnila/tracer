afbb695fd73c2a905c46d758744b1783a6625d0b
Daniel Trnka <daniel.trnka@gmail.com>
2016-10-02 21:39:58 +0200

    Fixed tests

--- TracedData.py
+++ TracedData.py
@@ -3,6 +3,61 @@ import os
 
 from json_encode import AppJSONEncoder
 
+import itertools
+
+
+class Process:
+    def __init__(self, data):
+        self.data = data
+
+    def __getitem__(self, item):
+        return self.data[item]
+
+    def items(self):
+        return self.data.items()
+
+    def get_resource_by(self, **kwargs):
+        for descriptor in self.data['descriptors']:
+            if kwargs and all(item in descriptor.items() for item in kwargs.items()):
+                return descriptor
+        return None
+
+
+class System:
+    def __init__(self, resource_path, data):
+        self.processes = {}
+        self.resource_path = resource_path
+
+        for id, process in data.items():
+            self.processes[int(id)] = Process(process)
+
+    def all_resources(self):
+        return list(itertools.chain(*[j['descriptors'] for i, j in self.processes.items()]))
+
+    def get_process_by(self, descriptors={}, **kwargs):
+        for pid, process in self.processes.items():
+            if descriptors:
+                for descriptor in process['descriptors']:
+                    if all(item in descriptor.items() for item in descriptors.items()):
+                        return process
+
+            if kwargs and all(item in process.items() for item in kwargs.items()):
+                return process
+        return None
+
+    def get_resource(self, id):
+        for i, proc in self.processes.items():
+            for type in ['read', 'write']:
+                for j, k in proc[type].items():
+                    if id == j:
+                        return k
+        return None
+
+    def get_process_by_pid(self, pid):
+        return self.processes[pid]
+
+    def read_file(self, id):
+        return open(self.resource_path + "/" + id, 'rb').read()
 
 class TracedData:
     def __init__(self, path):
