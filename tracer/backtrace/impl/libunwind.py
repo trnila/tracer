0d3e30088057a777595de3fcb32e13f5a757945a
Daniel Trnka <daniel.trnka@gmail.com>
2017-02-02 15:56:08 +0100

    Removed communicating with libunwind with structs library

--- tracer/backtrace/impl/libunwind.py
+++ tracer/backtrace/impl/libunwind.py
@@ -1,45 +1,15 @@
-import ctypes
-
-from tracer.utils import get_root
 from tracer.backtrace.addr2line import Addr2line
 from tracer.backtrace.backtrace import Frame
-
-
-class CPTrace:
-    def __init__(self):
-        self.lib = ctypes.CDLL(get_root() + "backtrace/backtrace.so")
-        self.lib.init()
-        self.lib.get_backtrace.restype = ctypes.POINTER(ctypes.c_long)
-
-    def destroy(self, pid=None):
-        if pid is None:
-            self.lib.destroy()
-        else:
-            self.lib.destroy_pid(pid)
-
-    def get_backtrace(self, pid):
-        data = self.lib.get_backtrace(pid)
-        casted = ctypes.cast(data, ctypes.POINTER(ctypes.c_long))
-
-        addresses = []
-        i = 0
-        while casted[i] != 0:
-            addresses.append(casted[i])
-            i += 1
-
-        return addresses
 
 
 class Libunwind:
     def __init__(self):
         import backtrace
         self.lib = backtrace
-        # self.lib = CPTrace()
         self.symbols = {}
 
     def __del__(self):
         self.lib.destroy()
-        pass
 
     def process_exited(self, pid):
         self.lib.destroy(pid)
