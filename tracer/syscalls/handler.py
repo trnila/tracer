846243da7d17f9508c4cdafba001aa74b7ab76fd
Daniel Trnka <daniel.trnka@gmail.com>
2017-02-04 17:07:28 +0100

    Removed not used metaclass

--- tracer/syscalls/handler.py
+++ tracer/syscalls/handler.py
@@ -18,32 +18,7 @@ class Event:
         return self.process.tracer
 
 
-class PluginMount(type):
-    def __init__(cls, name, bases, attrs, what):
-        if not hasattr(cls, 'plugins'):
-            # Called when the metaclass is first instantiated
-            cls.plugins = []
-        else:
-            # Called when a plugin class is imported
-            cls.register_plugin(cls)
-
-    def register_plugin(cls, plugin):
-        """Add the plugin to the plugin list and perform any registration logic"""
-
-        # create a plugin instance and store it
-        # optionally you could just store the plugin class and lazily instantiate
-        instance = plugin()
-
-        # save the plugin reference
-        cls.plugins.append(instance)
-
-        # apply plugin logic - in this case connect the plugin to blinker signals
-        # this must be defined in the derived class
-        instance.register_signals()
-
-
 class Extension:
-    __metaclass__ = PluginMount
     def on_start(self, tracer):
         pass
 
