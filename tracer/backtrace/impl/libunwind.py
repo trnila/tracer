4d22b898b1f7bd420d31f131578411dc9c15dec5
Daniel Trnka <daniel.trnka@gmail.com>
2017-02-04 19:33:45 +0100

    Libunwind: dont crash if obtaining backtrace failed, just log that error

--- tracer/backtrace/impl/libunwind.py
+++ tracer/backtrace/impl/libunwind.py
@@ -1,3 +1,5 @@
+import logging
+
 from tracer.backtrace.addr2line import Addr2line
 from tracer.backtrace.backtrace import Frame
 
@@ -16,18 +18,21 @@ class Libunwind:
 
     def create_backtrace(self, process):
         mappings = process.readMappings()
+        import backtrace
 
-        backtrace = []
-        for addr in self.lib.get_backtrace(process.pid):
-            for mapping in mappings:
-                if addr in mapping and not mapping.pathname.startswith('['):
-                    if mapping.pathname not in self.symbols:
-                        self.symbols[mapping.pathname] = Addr2line(mapping.pathname)
+        try:
+            backtrace_list = []
+            for addr in self.lib.get_backtrace(process.pid):
+                for mapping in mappings:
+                    if addr in mapping and not mapping.pathname.startswith('['):
+                        if mapping.pathname not in self.symbols:
+                            self.symbols[mapping.pathname] = Addr2line(mapping.pathname)
 
-                    addr = addr - mapping.start  # TODO: why relative address for code?
+                        addr = addr - mapping.start  # TODO: why relative address for code?
 
-                    resolved = self.symbols[mapping.pathname].resolve(addr)
-                    backtrace.append(Frame(addr, resolved if resolved else ""))
-                    break
-
-        return backtrace
+                        resolved = self.symbols[mapping.pathname].resolve(addr)
+                        backtrace_list.append(Frame(addr, resolved if resolved else ""))
+                        break
+            return backtrace_list
+        except backtrace.error as err:
+            logging.error("backtrace failed for pid %d: %s", process.pid, err)
