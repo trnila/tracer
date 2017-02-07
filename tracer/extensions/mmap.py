68d20fe55f17529847d9366025f89544200ea285
Daniel Trnka <daniel.trnka@gmail.com>
2017-02-07 11:55:09 +0100

    WIP: trace mmap

--- tracer/extensions/mmap.py
+++ tracer/extensions/mmap.py
@@ -1,4 +1,3 @@
-from tracer import fd
 from tracer.extensions.extension import Extension, register_syscall
 from tracer.mmap_tracer import MmapTracer
 
@@ -6,19 +5,31 @@ from tracer.mmap_tracer import MmapTracer
 class MmapExtension(Extension):
     def create_options(self, parser):
         parser.add_argument('--trace-mmap', action="store_true", default=False)
+        parser.add_argument('--save-mmap', action="store_true", default=False)
 
     @register_syscall("mmap")
     def mmap(self, syscall):
-        if syscall.arguments[4].value != 18446744073709551615:
-            syscall.process.mmap(syscall.arguments[4].value,
-                                 MmapTracer(syscall.process['pid'], syscall.result, syscall.arguments[1].value,
-                                            syscall.arguments[2].value,
-                                            syscall.arguments[3].value))
+        fd = syscall.arguments[4].value
+
+        if fd != 18446744073709551615:  # -1
+            mmap = MmapTracer(syscall.process['pid'], syscall.result, syscall.arguments[1].value,
+                              syscall.arguments[2].value,
+                              syscall.arguments[3].value)
+
+            syscall.process.mmap(syscall.arguments[4].value, mmap)
+            mmap.file = syscall.process.descriptors.get(fd)
 
     def on_tick(self, tracer):
-        if tracer.options.trace_mmap:
+        if tracer.options.trace_mmap or tracer.options.save_mmap:
             for pid, proc in tracer.data.processes.items():
                 for capture in proc['descriptors']:
                     if capture.descriptor.is_file and capture.descriptor['mmap']:
                         for mmap_area in capture.descriptor['mmap']:
-                            mmap_area.check()
+                            if tracer.options.trace_mmap:
+                                mmap_area.check()
+                            if tracer.options.save_mmap:
+                                if mmap_area.file['path'].endswith("100mb"):
+                                    f = open("/proc/{}/mem".format(mmap_area.pid), 'rb')
+                                    f.seek(mmap_area.start)
+                                    import code
+                                    code.interact(local=locals())
