d463be2b4b57b04a08ea72df4da050e7ca9c3db3
Daniel Trnka <daniel.trnka@gmail.com>
2017-03-17 11:08:16 +0100

    Tests: inherit added some checks

--- examples/mmap/inherit.c
+++ examples/mmap/inherit.c
@@ -1,9 +1,24 @@
 #include <stdio.h>
 #include <sys/mman.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <wait.h>
 
 int main() {
-	mmap(0, 128, PROT_READ, MAP_SHARED|MAP_ANONYMOUS, -1, 0);
-	mmap(0, 129, PROT_READ, MAP_PRIVATE|MAP_ANONYMOUS, -1, 0);
-	fork();
+	if(!mmap(0, 128, PROT_READ, MAP_SHARED|MAP_ANONYMOUS, -1, 0)) {
+	    perror("mmap 128");
+	    exit(1);
+	}
+	if(!mmap(0, 129, PROT_READ, MAP_PRIVATE|MAP_ANONYMOUS, -1, 0)) {
+	    perror("mmap 129");
+	    exit(1);
+	}
+
+	int pid = fork();
+    if(pid < 0) {
+        perror("fork");
+        exit(1);
+    } else if(pid != 0) {
+        while(wait(0) > 0);
+    }
 }
