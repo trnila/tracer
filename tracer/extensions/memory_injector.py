06e2222616cc047b2c48d079d0a637a900dcf357
Daniel Trnka <daniel.trnka@gmail.com>
2017-04-04 10:57:00 +0200

    MemoryInjector: factored out inject_mmap function that could be called everywhere to have own memory

--- tracer/extensions/memory_injector.py
+++ tracer/extensions/memory_injector.py
@@ -3,6 +3,51 @@ import mmap
 
 from tracer.extensions.extension import Extension
 
+SYSCALL_INSTR_SIZE = 2
+SYSCALL_MMAP = 9
+
+
+def inject_mmap(syscall, length, prot=mmap.PROT_READ | mmap.PROT_WRITE, flags=mmap.MAP_ANONYMOUS | mmap.MAP_PRIVATE):
+    """
+        create address space of length in traced process, returns address in traced process
+        currently injecting memory works for process that is *before* syscall, ie. syscall.result returns None
+    """
+    if syscall.result is not None:
+        raise RuntimeError("Process must be before syscall, ie. syscall.result must return None")
+
+    backup = Backup()
+    p = syscall.process.tracer.backend.debugger[syscall.process.pid]
+
+    # backup and prepare new register with mmap syscall
+    regs = p.getregs()
+    backup.backup(regs)
+    regs.orig_rax = SYSCALL_MMAP
+    regs.rdi = 0
+    regs.rsi = length
+    regs.rdx = prot
+    regs.r10 = flags
+    regs.r8 = -1
+    regs.r9 = 0
+
+    # set modified registers, resume and wait for syscall
+    p.setregs(regs)
+    p.syscall()
+    p.waitSyscall()
+
+    # get mmap address
+    addr = p.getregs().rax
+
+    # set instruction pointer before original syscall, resume and wait
+    p.setInstrPointer(p.getInstrPointer() - SYSCALL_INSTR_SIZE)
+    p.syscall()
+    p.waitSyscall()
+
+    # restore original registers
+    backup.restore(regs)
+    p.setregs(regs)
+
+    return addr
+
 
 class Backup:
     def __init__(self):
@@ -21,46 +66,13 @@ class Backup:
 class MemoryInjectorExtension(Extension):
     """  Extension injects mmap region to each process at start """
 
-    SYSCALL_INSTR_SIZE = 2
-    SYSCALL_MMAP = 9
-
     def __init__(self):
         super().__init__()
         self.injected = set()
 
     def on_syscall(self, syscall):
         if syscall.process.pid not in self.injected:
-            backup = Backup()
-            p = syscall.process.tracer.backend.debugger[syscall.process.pid]
-
-            # backup and prepare new register with mmap syscall
-            regs = p.getregs()
-            backup.backup(regs)
-            regs.orig_rax = self.SYSCALL_MMAP
-            regs.rdi = 0
-            regs.rsi = 1024
-            regs.rdx = mmap.PROT_READ | mmap.PROT_WRITE
-            regs.r10 = mmap.MAP_ANONYMOUS | mmap.MAP_PRIVATE
-            regs.r8 = -1
-            regs.r9 = 0
-
-            # set modified registers, resume and wait for syscall
-            p.setregs(regs)
-            p.syscall()
-            p.waitSyscall()
-
-            # get mmap address
-            addr = p.getregs().rax
+            addr = inject_mmap(syscall, 1024)
             syscall.process['mem'] = addr
-            logging.debug("[%d] our mmap memory located at %X", p.pid, addr)
-
-            # set instruction pointer before original syscall, resume and wait
-            p.setInstrPointer(p.getInstrPointer() - self.SYSCALL_INSTR_SIZE)
-            p.syscall()
-            p.waitSyscall()
-
-            # restore original registers
-            backup.restore(regs)
-            p.setregs(regs)
-
+            logging.debug("[%d] our mmap memory located at %X", syscall.process.pid, addr)
             self.injected.add(syscall.process.pid)
