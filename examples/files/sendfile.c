6a0f36aa111875b93e86b996e9800012278ae493
Daniel Trnka <daniel.trnka@gmail.com>
2017-04-04 15:05:46 +0200

    Example: added files/sendfile.c

---
+++ examples/files/sendfile.c
@@ -0,0 +1,23 @@
+#include <sys/sendfile.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <sys/stat.h>
+
+int main() {
+	int fd = open("/etc/passwd", O_RDONLY);
+	if(fd < 0) {
+		perror("open");
+		exit(1);
+	}
+
+	struct stat buf;
+	if(fstat(fd, &buf) != 0) {
+		perror("fstat");
+		exit(1);
+	}
+	sendfile(1, fd, NULL, buf.st_size);
+
+	return 0;
+}
