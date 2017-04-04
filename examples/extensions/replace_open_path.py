540989e2564259c79bc8921b9b6a225879ed8177
Daniel Trnka <daniel.trnka@gmail.com>
2017-04-04 09:36:00 +0200

    WIP: inject mmap syscall

--- examples/extensions/replace_open_path.py
+++ examples/extensions/replace_open_path.py
@@ -1,6 +1,8 @@
 import argparse
 import logging
 
+import mmap
+
 from tracer.extensions.extension import Extension, register_syscall
 
 
@@ -26,7 +28,7 @@ class ChangeOpenPath(Extension):
             default={}
         )
 
-    @register_syscall("open", success_only=False)
+    #@register_syscall("open", success_only=False)
     def change(self, syscall):
         paths = syscall.process.tracer.options.replace_path
         requested_path = syscall.arguments[0].text
@@ -39,3 +41,84 @@ class ChangeOpenPath(Extension):
 
         addr = syscall.arguments[0].value
         syscall.process.write_bytes(addr, new_path.encode('utf-8') + b'\0')
+
+
+# d=syscall.process.tracer.backend.debugger
+# d[process.pid].setInstrPointer(0x555555554000 + 0x7f0)
+# d[process.pid].cont()
+
+class Backup:
+    def __init__(self):
+        self.registers = {}
+
+    def backup(self, regs):
+        for prop in dir(regs): #['orig_rax', 'rdi', 'rsi', 'rdx', 'r10', 'r8', 'r9']: #dir(regs):
+            if not prop.startswith('_'):
+                self.registers[prop] = getattr(regs, prop)
+
+    def restore(self, regs):
+        for key, val in self.registers.items():
+            setattr(regs, key, val)
+
+class Test(Extension):
+    def __init__(self):
+        super().__init__()
+        self.injected = set()
+
+    def on_syscall(self, syscall):
+        if syscall.name != 'open' or syscall.arguments[0].text != "/etc/passwd" or syscall.result is not None:
+            return
+
+
+        backup = Backup()
+        if syscall.process.pid not in self.injected:
+            p=syscall.process.tracer.backend.debugger[syscall.process.pid]
+
+            pre = p.getInstrPointer()
+            print(hex(p.getInstrPointer()))
+
+            # backup and prepare new register with mmap syscall
+            regs = p.getregs()
+            backup.backup(regs)
+            regs.orig_rax = 9
+            regs.rdi = 0
+            regs.rsi = 1024
+            regs.rdx = mmap.PROT_READ | mmap.PROT_WRITE
+            regs.r10 = mmap.MAP_ANONYMOUS | mmap.MAP_PRIVATE
+            regs.r8 = -1
+            regs.r9 = 0
+
+            # resume and wait for syscall
+            p.setregs(regs)
+            p.syscall()
+            p.waitSyscall()
+            print('ok injected')
+
+            print(hex(p.getInstrPointer()))
+
+            # get mmap region
+            addr = p.getregs().rax
+            logging.info("%d mmap located at %X", p.pid, addr)
+
+            # part 2
+            p.setInstrPointer(pre - 2)
+            p.syscall()
+            p.waitSyscall()
+            print('we can continue')
+
+            backup.restore(regs)
+            p.setregs(regs)
+
+            self.injected.add(syscall.process.pid)
+            syscall.process['mem'] = addr
+
+
+
+            #p.setInstrPointer(before_syscall_addr)
+            #p.syscall()
+
+            #import struct
+            #val = p.getregs().rax.to_bytes(8, byteorder='little')
+            #print(struct.unpack("l", val))
+
+            #from IPython import embed; embed()
