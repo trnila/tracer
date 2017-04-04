c72459b82474d1185fb4b802f2901eb7e9b95d61
Daniel Trnka <daniel.trnka@gmail.com>
2017-04-04 13:04:31 +0200

    InjectWrite: docs

--- examples/extensions/inject_write.py
+++ examples/extensions/inject_write.py
@@ -1,5 +1,4 @@
 import mmap
-
 import struct
 
 from tracer.extensions.extension import Extension, register_syscall
@@ -9,13 +8,17 @@ SYSCALL_NOOP = 102  # getuid, NOOP syscall that should have no side effects and 
 
 
 class InjectWrite(Extension):
+    """
+    Inject code that does writing to stdout before each mmap and then continue with executing mmap
+    """
+
     CODE_SET_SYSCALL = b"\xb8\x01\x00\x00\x00"     # mov 1, %eax
     CODE_SET_DESCRIPTOR = b"\xbf\x01\x00\x00\x00"  # mov 1, %edi
     CODE_SET_BUF = b"\x48\xbe"                     # mov ..., %rsi
     CODE_SET_LEN = b"\x48\xba"                     # mov ..., %rdx
     CODE_SYSCALL = b"\x0f\x05"                     # syscall
 
-    @register_syscall("sendfile", success_only=False)
+    @register_syscall("mmap", success_only=False)
     def fn(self, syscall):
         if syscall.result is not None:
             return
@@ -29,37 +32,43 @@ class InjectWrite(Extension):
         backup = Backup()
         backup.backup(p.getregs())
 
-
-
-        # prepare buffer for text that will be written
+        # prepare buffer for text that will be written to stdout
         buffer = inject_mmap(syscall, 1024)
         proc.write_bytes(buffer, text)
 
-        # create instructions on fly
+        # create instructions for write(0, buffer, size) on fly
         code = self.CODE_SET_SYSCALL + \
                self.CODE_SET_DESCRIPTOR + \
                self.CODE_SET_LEN + struct.pack("<q", len(text)) + \
                self.CODE_SET_BUF + struct.pack("<q", buffer) + \
                self.CODE_SYSCALL
 
-        # prepare region for code
+        # prepare region for code instructions
         addr = inject_mmap(syscall, 1024, prot=mmap.PROT_READ | mmap.PROT_WRITE | mmap.PROT_EXEC)
         proc.write_bytes(addr, code)
 
+        # jump to injected code
         p.setInstrPointer(addr)
+
+        # replace syscall with NOOP syscall
         regs = p.getregs()
         regs.orig_rax = SYSCALL_NOOP
         p.setregs(regs)
 
+        # execute all instructions that we have written to injected memory
+        # XXX: performance: maybe we can insert syscall(SIGTRAP)
         while p.getInstrPointer() < addr + len(code):
-            print(p.getInstrPointer())
             p.singleStep()
             p.waitEvent()
 
+        # go back before original syscall
         p.setInstrPointer(orig_ip - 2)
         p.syscall()
         p.waitSyscall()
 
+        # we are before syscall, restore registers
         regs = p.getregs()
         backup.restore(regs)
         p.setregs(regs)
+
+        # XXX: we shall unmap memory regions!
