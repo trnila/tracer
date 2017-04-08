adc31ab47f998a74c4262f34ff17a1e4604e6537
Daniel Trnka <daniel.trnka@gmail.com>
2017-04-08 13:42:15 +0200

    mmap: flat test with memory sharing

--- examples/mmap/mmap_memory.c
+++ examples/mmap/mmap_memory.c
@@ -1,40 +1,11 @@
-#include <sys/mman.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <stdio.h>
-#include <wait.h>
-#include <string.h>
-#include <stdlib.h>
-#include <errno.h>
-
-const char *name = "test";
-
-char* get_mem() {
-	int size = 1024;
-
-	int fd = shm_open(name,  O_CREAT | O_RDWR | O_EXCL, 0600);
-	if(fd == -1) {
-		if(errno == EEXIST) {
-			fd = shm_open(name, O_RDWR, 0600);
-			if(fd == -1) {
-				perror("shm_open 2");
-			}
-		} else {
-			perror("shm_open");
-		}
-	} else {
-		ftruncate(fd, size);
-	}
-
-	char* addr = mmap(0, size, PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0);
-	printf("Got address 0x%X\n", addr);
-
-	return addr;
-}
+#include "common.h"
 
 int main() {
+    const char name[] = "test";
+    int size = 1024;
+
 	if(fork() == 0) {
-		char* mem = get_mem();
+		char* mem = create_share_mem(name, size);
 
 		while(strcmp("hello", mem)) {
 			printf(".");
@@ -45,14 +16,12 @@ int main() {
 		exit(0);
 	}
 
-	char* mem = get_mem();
+	char* mem = create_share_mem(name, size);
 
 	usleep(250000);
 	strcpy(mem, "hello");
 
 	while(wait(NULL) > 0);
-
-
 
 	shm_unlink(name);
 
