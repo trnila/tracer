9d4978eb55b2c81903644efb0062de2bdff600f3
Daniel Trnka <daniel.trnka@gmail.com>
2017-04-24 07:19:19 +0200

    Fixed: multi scripts

--- examples/multi/multi_http.sh
+++ examples/multi/multi_http.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 tracer -o /tmp/report1 -- timeout 60 python -m http.server &
+PID=$!
 sleep 15
-tracer -o /tmp/report2 -- sh -c "echo GET /Readme.md | nc localhost 8000"
-tracer -o /tmp/report3 -- curl localhost:8000/Makefile
+echo "running clients"
+tracer -o /tmp/report2 -- bash -c "echo GET /multi_nc.sh | nc localhost 8000"
+tracer -o /tmp/report3 -- curl localhost:8000/remote_nc.sh
+
+echo "wait until python http server will shut down"
+wait $PID
+echo "now you can run tracerui /tmp/report{1,2,3}"
